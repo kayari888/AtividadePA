ab3c581c023d71413358a09005604dd0f2d12516
kayari888 <kayarimelissa@gmail.com>
2026-06-28 18:00:08 -0300

    Versão final

--- src/App.java
+++ src/App.java
@@ -2,110 +2,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
-<<<<<<< HEAD
-    public static void main(String[] args) {
-
-        Scanner inUser = new Scanner(System.in);
-        ArrayList<String> listaCompras = new ArrayList<String>();
-
-        int opcaoLista = 0;
-        int opcao = 0;
-        String novoItem;
-
-        do {
-            try {
-                System.out.println(
-                        "Bem vindos ao programa lista de compras\n\n" +
-                        "Escolha a opcao desejada\n\n" +
-                        "1 - Criar uma nova lista de compras\n" +
-                        "2 - Sair\n");
-                opcaoLista = inUser.nextInt();
-                inUser.nextLine();
-
-                switch (opcaoLista) {
-                    case 1:
-                        System.out.println("Lista criada com sucesso!");
-
-                        do {
-                            System.out.println(
-                                    "Escolha a opcao desejada:\n" +
-                                    "1 - Adicionar um item a lista\n" +
-                                    "2 - Remover um item a lista\n" +
-                                    "3 - Exibir a lista de compras\n" +
-                                    "4 - Limpar a lista de compras\n" +
-                                    "5 - Alterar item\n" +
-                                    "6 - Voltar ao menu principal\n");
-
-                            opcao = inUser.nextInt();
-                            inUser.nextLine();
-
-                            switch (opcao) {
-                                case 1:
-                                    System.out.println("Voce escolheu criar um item da lista");
-                                    System.out.println("Digite o nome do produto");
-                                    novoItem = inUser.nextLine();
-                                    listaCompras.add(novoItem);
-                                    break;
-                                case 2:
-                                    System.out.println("Voce escolheu remover um item:");
-                                    System.out.println("Digite o nome do produto para remover:");
-                                    String comprasRemover = inUser.nextLine();
-                                    boolean removido = listaCompras.remove(comprasRemover);
-                                    if (removido) {
-                                        System.out.println("Compra removida com sucesso!");
-                                    } else {
-                                        System.out.println("Item nao encontrado.");
-                                    }
-                                    break;
-                                case 3:
-                                    System.out.println("Voce escolheu ver a lista");
-                                    for (int i = 0; i < listaCompras.size(); i++) {
-                                        System.out.println((i + 1) + " - " + listaCompras.get(i));
-                                    }
-                                    break;
-                                case 4:
-                                    listaCompras.clear();
-                                    System.out.println("Lista de compras limpa com sucesso!");
-                                    break;
-                                case 5:
-                                    System.out.println("Digite o nome do item que deseja alterar:");
-                                    String itemAntigo = inUser.nextLine();
-                                    int indice = listaCompras.indexOf(itemAntigo);
-                                    if (indice != -1) {
-                                        System.out.println("Digite o novo nome do item:");
-                                        String itemNovo = inUser.nextLine();
-                                        listaCompras.set(indice, itemNovo);
-                                        System.out.println("Item alterado com sucesso!");
-                                    } else {
-                                        System.out.println("Item nao encontrado.");
-                                    }
-                                    break;
-                                case 6:
-                                    System.out.println("Voltando ao menu principal...");
-                                    break;
-                                default:
-                                    System.out.println("Opcao invalida");
-                                    break;
-                            }
-                        } while (opcao != 6);
-                        break;
-                    case 2:
-                        System.out.println("Obrigada por utilizar nosso sistema.");
-                        break;
-                    default:
-                        System.out.println("Opcao invalida");
-                        break;
-                }
-            } catch (Exception erro) {
-                System.out.println("Erro na entrada. Tente novamente.");
-                inUser.nextLine();
-                opcaoLista = 1;
-            }
-        } while (opcaoLista != 2);
-
-        inUser.close();
-    }
-=======
     public static void main(String[] args) throws Exception {
         Scanner inUser = new Scanner (System.in);
          ArrayList<String> listaCompras = new ArrayList<>();
@@ -114,7 +10,7 @@ public class App {
     int opcao = 0;
     String novoItem;
     int contadora = 0; 
-    // não tenho certeza sobre a contadora
+    
     
 
     
@@ -139,7 +35,7 @@ public class App {
                 do{
                     System.out.println(""" 
                         escolha a opcao desejada:
-                        1 - Adicionar aum item a lista
+                        1 - Adicionar um item a lista
                         2 - Remover um item a lista
                         3 - Exibir a lista de compras
                         4 - Limpar a lista de compras
@@ -161,7 +57,7 @@ public class App {
                         }
 
                           case 2 -> {
-                                    System.out.println("Você escolheu remover uma atividade:");
+                                    System.out.println("Você escolheu remover um item:");
                                     String comprasRemover = inUser.nextLine();
 
                                     boolean removido = listaCompras.remove(comprasRemover);
@@ -177,9 +73,8 @@ public class App {
 
                         case 3 ->{
                             System.out.println("Você escolheu ver a lista");
-                            for(String item : listaCompras){
-                                System.out.printf("%d %s \n", contadora, item);
-                                contadora++;
+                            for(int i =0 ; i < listaCompras.size(); i++){
+                                System.out.println((i+1) + " - " + listaCompras.get(i));
                             }
                         }
 
@@ -189,13 +84,27 @@ public class App {
     }
 
                     case 5 -> {
-            
+                         System.out.println("Digite o nome do item que deseja alterar:");
+                         String itemAntigo = inUser.nextLine();
+
+                        int indice = listaCompras.indexOf(itemAntigo);
+
+                        if (indice != -1) {
+                        System.out.println("Digite o novo nome do item:");
+                        String itemNovo = inUser.nextLine();
+
+                        listaCompras.set(indice, itemNovo);
+
+                          System.out.println("Item alterado com sucesso!");
+                          } else {
+                            System.out.println("Item não encontrado.");
+                              }   
 }
 
 
                     }
 
-                }while(opcao != 5);
+                }while(opcao != 6);
 
 
             }
@@ -216,5 +125,4 @@ public class App {
     }
       
   
->>>>>>> 17f81683ae9edd9a64fdf3fcf85072df4d3ceb1c
 }
